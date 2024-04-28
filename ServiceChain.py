d5075f9d718ab85e9778a64e6a8c230a1d235e81
Merijn <merijn.laks@student.uva.nl>
2024-04-28 22:36:21 +0200

    feat: create service chain class

---
+++ ServiceChain.py
@@ -0,0 +1,11 @@
+from NetworkFunction import NetworkFunction
+
+
+class ServiceChain:
+    functions: list[NetworkFunction] = []
+
+    def add_function(self, function: NetworkFunction):
+        pass
+
+    def calculate_latency(self):
+        pass
