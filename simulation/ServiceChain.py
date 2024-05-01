cf9b939935292402e93d388f8f0641ed7e1c8544
Merijn <merijn.laks@student.uva.nl>
2024-05-01 15:21:59 +0200

    feat: add time to live and functionality to chain chains

--- simulation/ServiceChain.py
+++ simulation/ServiceChain.py
@@ -1,11 +1,23 @@
+from __future__ import annotations  # used for forward reference
+
 from simulation.NetworkFunction import NetworkFunction
 
 
 class ServiceChain:
+    """
+    A service chain consists of multiple Network Functions in an ordered list.
+    When the chain gets embedded it will be in the order of the functions list.
+    """
     functions: list[NetworkFunction] = []
 
-    def add_function(self, function: NetworkFunction):
-        pass
+    # by defining a next chain, we make it possible to support
+    # traffic splitting and bifurcated paths
+    next_chain: list[ServiceChain] = []
+
+    time_to_live: float = 0  # time until which this chain should be available
+
+    def add_function(self, function: NetworkFunction) -> None:
+        self.functions.append(function)
 
     def calculate_latency(self) -> float:
         pass
