ae08641d904401471cdbbe2101a3a45213a1f4aa
Merijn <merijn.laks@student.uva.nl>
2024-05-01 15:15:20 +0200

    feat: define traffic generator structure

--- TrafficGenerator.py
+++ TrafficGenerator.py
@@ -1,2 +1,35 @@
+from simulation import ServiceChain
+from simulation import NetworkFunction
+
+"""
+This is a dummy implementation of a Traffic generator,
+we could change this into an interface to allow for multiple 
+implementations.
+"""
+
 class TrafficGenerator:
-    pass
+    """
+    This class generates random function chain requests consisting of random network functions
+    """
+
+    def __init__(self):
+        pass
+
+    def create_request_chain(self):
+        chain = ServiceChain()
+
+        chain.add_function(NetworkFunction(
+            cpu_usage=500,
+            memory_usage=200,
+            storage_usage=512,
+            processing_time=5
+        ))
+
+        chain.add_function(NetworkFunction(
+            cpu_usage=500,
+            memory_usage=200,
+            storage_usage=512,
+            processing_time=5
+        ))
+
+        return chain
